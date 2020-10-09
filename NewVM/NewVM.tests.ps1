1ad579b81575ed246027fe2f8650b308f60e3d4b
Mark Kerry <mark.kerry@concentra.co.uk>
2020-10-09 09:29:32 +0100

    first commit

---
+++ NewVM/NewVM.tests.ps1
@@ -0,0 +1,46 @@
+<#param (
+    # Parameter help description
+    [Parameter(Mandatory=$true)]
+    [string]$TemplatePath
+)
+#>
+
+BeforeAll {
+    $TemplatePath = "C:\Git\Arm\NewVM.json"
+    $templateRaw = Get-Content -Path $TemplatePath -Raw -ErrorAction SilentlyContinue
+    $template = ConvertFrom-Json -InputObject $templateRaw -ErrorAction SilentlyContinue
+}
+
+Describe 'ARM Template Validation' {
+    Context 'File Validation' {
+        It 'Template ARM file exists' {
+            Test-Path $TemplatePath -Include "*.json" | Should -Be $true
+        }
+        It 'Is a valid JSON file' {
+            $templateRaw | ConvertFrom-Json -ErrorAction SilentlyContinue | Should -Not -Be $null
+        }
+    }
+    Context 'Template Content Validation' {
+        It 'Contains all required elements' {
+            $elements = '$schema',
+                        'contentVersion',
+                        'functions',
+                        'outputs',
+                        'parameters',
+                        'resources',
+                        'variables'
+            $templateElements = $template | Get-Member -MemberType NoteProperty | ForEach-Object Name
+            $templateElements | Should -Be $elements
+        }
+        It 'Creates the expected resources' {
+            $resources = 'Microsoft.Storage/storageAccounts',
+                        'Microsoft.Network/publicIPAddresses',
+                        'Microsoft.Network/networkSecurityGroups',
+                        'Microsoft.Network/virtualNetworks',
+                        'Microsoft.Network/networkInterfaces',
+                        'Microsoft.Compute/virtualMachines'
+            $templateResources = $template.resources.type
+            $templateResources | Should -Be $resources
+        }
+    }
+}
